fe683ce62274829336d5006d945853987d7b81d1
JoshNells13 <joshua.plk08@gmail.com>
2025-12-30 21:36:13 +0700

    Install Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,68 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CartController;
-use App\Http\Controllers\ProductTransactionController;
-use App\Http\Controllers\TransactionDetailController;
-use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Public Routes
-|--------------------------------------------------------------------------
-*/
 Route::get('/', function () {
     return view('welcome');
 });
 
-/*
-|--------------------------------------------------------------------------
-| Auth Routes
-|--------------------------------------------------------------------------
-*/
-Auth::routes();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-
-Route::middleware(['auth'])->group(function () {
-
-
-    Route::middleware(['role:admin'])->prefix('admin')->group(function () {
-
-
-        Route::view('/dashboard', 'admin.dashboard')->name('admin.dashboard');
-
-        Route::resource('categories', CategoryController::class);
-
-        Route::resource('products', ProductController::class);
-
-
-        Route::get('/transactions', [ProductTransactionController::class, 'index'])
-            ->name('admin.transactions.index');
-
-        Route::get('/transactions/{transaction}', [TransactionDetailController::class, 'show'])
-            ->name('admin.transactions.show');
-    });
-
-
-    Route::middleware(['role:pembeli'])->group(function () {
-
-        // Product browsing
-        Route::get('/products', [ProductController::class, 'index'])
-            ->name('products.index');
-
-        Route::get('/products/{product}', [ProductController::class, 'show'])
-            ->name('products.show');
-
-        // Cart
-        Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-        Route::post('/cart', [CartController::class, 'store'])->name('cart.store');
-        Route::delete('/cart/{cart}', [CartController::class, 'destroy'])->name('cart.destroy');
-
-        // Checkout
-        Route::post('/checkout', [ProductTransactionController::class, 'store'])
-            ->name('checkout.store');
-    });
-
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
